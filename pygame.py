d71faa5cf5b83e9a588b2b1123298148fb130e42
Mark Amber <me@markamber.co>
2011-06-19 22:46:25 -0500

    added caps

--- pygame.py
+++ pygame.py
@@ -38,9 +38,9 @@ def levelselector():
 def ask_ok(prompt, retries=4, complaint='Yes or no, please!'):
     while True:
         ok = raw_input(prompt)
-        if ok in ('y', 'ye', 'yes'):
+        if ok in ('y', 'ye', 'yes', 'Y', 'YE', 'YES'):
             return True
-        if ok in ('n', 'no', 'nop', 'nope'):
+        if ok in ('n', 'no', 'nop', 'nope', 'N', 'NO', 'NOP', 'NOPE'):
             return False
         retries = retries - 1
         if retries < 0:
