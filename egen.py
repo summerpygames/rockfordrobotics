513e0db2fcf3901a48d555c0be45d9bc488653c3
lukedmor <lukedmor@me.com>
2011-06-24 16:37:18 -0500

    adjusted 1,2,3,4 to 0,1,2,3

--- egen.py
+++ egen.py
@@ -9,27 +9,27 @@ from math import floor
 def egen(size, i, offset = 0):
     position=[0,0]
     if size[1] <= 450:        
-        if i == 1:
+        if i == 0:
             position[0] = floor(size[0]-100+offset)
-        elif i == 2:
+        elif i == 1:
             position[0] = floor(size[0]-100+offset)
             position[1] = floor(size[1]/2)
-        elif i == 3:
+        elif i == 2:
             position[0] = floor(size[0]-200+offset)
             position[1] = floor(100)
-        elif i == 4:
+        elif i == 3:
             position[0] = floor(size[0]-200+offset)
             position[1] = floor(size[1]/2+100)
     elif size[1] <= 650:
-        if i == 1:
+        if i == 0:
             position[0] = floor(size[0]-100+offset)
-        elif i == 2:
+        elif i == 1:
             position[0] = floor(size[0]-100+offset)
             position[1] = floor(size[1]-100)
-        elif i == 3:
+        elif i == 2:
             position[0] = floor(size[0]-200+offset)
             position[1] = floor(size[1]*3/4-100)
-        elif i == 4:
+        elif i == 3:
             position[0] = floor(size[0]-200+offset)
-            position[1] = floor(size[1]/4-200)
+            position[1] = floor(size[1]/2-size[1]/4-200)
     return (position[0],position[1])
