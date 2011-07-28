578b0ef4ad42a450227cd8c8875048524a9f3798
Mark Amber <me@markamber.co>
2011-07-28 02:23:50 -0500

    Good days work on the menu

--- menu.py
+++ menu.py
@@ -23,7 +23,7 @@ from APH.Screen import *
 from APH.Sprite import *
 
 import menuassets
-from olpcgames import svgsprite
+from olpcgames import svgsprite textsprite
 import olpcgames
 import pygame
 import logging
@@ -33,12 +33,12 @@ import logging
 ##  For the Main Menu                    ##
 ###########################################
 
-main = (
-        (('play',  'D'),)
-        (('how',  'UD'),)
-        (('cred', 'UD'),)
-        (('about', 'U'),)
-       )
+mainmen = (
+           (('play',  'D'),)
+           (('how',  'UD'),)
+           (('cred', 'UD'),)
+           (('about', 'U'),)
+          )
 
 ###########################################
 ##  For the Grades menu                  ##
@@ -82,9 +82,11 @@ how = ((('none', ''),),)
 ###Classes shared between states###
 class Button(Sprite):
     """Base button class to be built upon"""
-    def __init__(self, position = (0, 0), sel_svg=None, des_svg=None,
-                 size=None, copy=False,
-                 callout='NO', initsel=False):
+    def __init__(self, position = (0, 0), #starting position?
+                 sel_svg=None, des_svg=None, # images
+                 size=None, # only spec the vertical/hor size
+                 callout='NO', # Name to respond to select
+                 initsel=False):  # Select this on start
         
         data_sel = open(sel_svg).read()
         data_des = open(des_svg).read()
@@ -94,12 +96,12 @@ class Button(Sprite):
 
         self.selected = initsel
         super(MovingSvgObject, self).__init__()
-        if self.selected is True
-            self.
-        
+        self.image = new_surface(self.sprite_sel.image.get_size())
+        if self.selected is True:
+            self.image.blit(self.sprite_sel.image, (0, 0))
+        else:
+            self.image.blit(self.sprite_des.image, (0, 0))
 
-        self.image = new_surface(self.sprite.image.get_size())
-        self.image.blit(self.sprite.image, (0, 0))
         self.rect = self.sprite.rect
         self.resolution = self.sprite.resolution
         self.position = (position[0], position[1])
@@ -109,7 +111,40 @@ class Button(Sprite):
     def update(self, callout):
         """This will set the button to deselected if it was the thing
         that is now selected, and it will make itself selected if it is"""
-        pass
+        if callout == self.callout:
+            self.image.blit(self.sprite_sel.image, (0, 0))
+        else:
+            self.image.blit(self.sprite_des.image, (0, 0))
+
+def keys(event, action):
+    """A little hack to make it easier to use the other parts of the programs, I
+    think it is a little unneccecary, but it is OK, just shows how you can make
+    the program take more room or something
+    """
+    if action == 'escape':
+        if event.key == pygame.K_ESCAPE:
+            return True
+    elif action == 'left':
+        if event.key == pygame.K_KP4 or event.key == pygame.K_LEFT:
+            return True
+    elif action == 'right':
+        if event.key == pygame.K_KP6 or event.key == pygame.K_RIGHT:
+            return True
+    elif action == 'up':
+        if event.key == pygame.K_KP8 or event.key == pygame.K_UP:
+            return True
+    elif action == 'down':
+        if event.key == pygame.K_KP2 or event.key == pygame.K_DOWN:
+            return True
+    elif action == 'next':
+        if event.key == pygame.K_KP1 or event.key == pygame.K_SPACE:
+            return True
+    elif action == 'back':
+        if event.key == pygame.K_KP3 or event.key == pygame.K_x:
+            return True
+    else:
+        return False
+
 
 ###Classes for instructions###
 class MainMenu(SubGame):
@@ -124,17 +159,20 @@ class MainMenu(SubGame):
             return
         self.cursor = [0, 0]
         self.initialized = True
-        self.set_layers(['instructions'])
+        self.set_layers(['main'])
         self.t = 0        
         #Background initialization
-        bg = my_load_image('instructionScreen.jpg')
+        bg = load_image(os.path.join('data', 'spacebg.jpg'))
         self.screen_state.set_background(bg)
         #Font & text initialization
         pygame.font.init()
         self.font = pygame.font.SysFont(None,80)
         #Sprite initialization
+        self.play = Button()
+        self.howto = Button()
+        self.about = Button()
+        self.credits = Button()
 
-        self.backButton = baseButton('returnButton.png',(450,virtualHEIGHT-150))
         #Sprite group initialization
         self.group = Group()
 
@@ -142,14 +180,29 @@ class MainMenu(SubGame):
         self.t = self.t + 1
         self.group.draw()
         GetScreen().draw()
-        #Handle Button Clicks
-        for event in Mouse.get_events():
-            if self.backButton.detectCollision(event):
-                self.pop_state()
         #Handle Other Events
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
                 self.pop_state()
             elif event.type == KEYDOWN and event.key == K_RETURN:
                 self.pop_state()
+            elif (keys(event, 'up') and
+                  'U' in mainmen[self.cursor[0]][self.cursor[1]][1]):
+                self.cursor[0] += 1
+                self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+            elif (keys(event, 'down') and
+                  'D' in mainmen[self.cursor[0]][self.cursor[1]][1]):
+                self.cursor[0] -= 1
+                self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+            elif (keys(event, 'left') and
+                  'L' in mainmen[self.cursor[0]][self.cursor[1]][1]):
+                self.cursor[1] -= 1
+                self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+            elif (keys(event, 'right') and
+                  'R' in mainmen[self.cursor[0]][self.cursor[1]][1]):
+                self.cursor[1] += 1
+                self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+
+
+            
 
