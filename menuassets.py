27fb5821a670abbe6ebb8e642a1bcff2cef03890
markamber <me@markamber.co>
2011-07-27 23:02:20 -0500

    Added menuassets, for modification by eli

---
+++ menuassets.py
@@ -0,0 +1,61 @@
+import os
+assets = 'data'
+button = 'button'
+allign = 'menu'
+
+##########################
+## To complete this file##
+##  REPLACE the '.svg'  ##
+##########################
+# EDIT THIS IN NOTEPAD++ #
+##########################
+
+###########################################
+##  For the Main Menu                    ##
+###########################################
+
+main_allign = os.path.join(assets, allign, '.svg')
+main_play = os.path.join(assets, button, '.svg')
+main_howto = os.path.join(assets, button, '.svg')
+main_credits = os.path.join(assets, button, '.svg')
+main_about = os.path.join(assets, button, '.svg')
+
+
+###########################################
+##  For the Grades menu                  ##
+###########################################
+
+grades_allign = os.path.join(assets, allign, '.svg')
+grades_1 = os.path.join(assets, button, '.svg')
+grades_2 = os.path.join(assets, button, '.svg')
+grades_3 = os.path.join(assets, button, '.svg')
+
+
+###########################################
+##  For the How to Play menu             ##
+###########################################
+
+htpl_allign = os.path.join(assets, allign, '.svg')
+
+###########################################
+##  For the Stage Selection Menu         ##
+###########################################
+
+stage_allign = os.path.join(assets, allign, '.svg')
+stage_play = os.path.join(assets, button, '.svg')
+stage_deep = os.path.join(assets, button, '.svg')
+stage_solar = os.path.join(assets, button, '.svg')
+stage_planet = os.path.join(assets, button, '.svg')
+stage_city = os.path.join(assets, button, '.svg')
+
+###########################################
+##  For the About the Team Menu          ##
+###########################################
+
+about_allign = os.path.join(assets, allign, '.svg')
+
+###########################################
+##  For the Credits Menu                 ##
+###########################################
+
+credits_align = os.path.join(assets, allign, '.svg')
