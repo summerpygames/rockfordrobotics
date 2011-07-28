c101189142d37bf88caf540416e603462e7aac37
Mark Amber <me@markamber.co>
2011-07-28 16:54:43 -0500

    2 Menus work now

--- menu.py
+++ menu.py
@@ -97,7 +97,9 @@ class Allignment(Sprite):
     def update(self, *args):
         """do nothing"""
         pass
-
+    
+    def trigger(self, *args):
+        pass
         
 
 class Button(Sprite):
@@ -113,7 +115,6 @@ class Button(Sprite):
         data_des = open(des_svg).read()
         self.sprite_sel = svgsprite.SVGSprite(svg=data_sel, size=size)
         self.sprite_des = svgsprite.SVGSprite(svg=data_des, size=size)
-        print self.sprite_sel.image.get_size()
 
         self.selected = initsel
         Sprite.__init__(self)
@@ -129,7 +130,7 @@ class Button(Sprite):
         self.callout = callout
         self.attrigger = trigger
 
-    def trigger(self):
+    def trigger(self, callout):
         """What state to transfer to when the button is pressed"""
         if callout == self.callout:
             return self.attrigger
@@ -142,7 +143,6 @@ class Button(Sprite):
         if callout == self.callout:
             self.image = self.sprite_sel.image
             self.selected = True
-            print 'update to', self.callout
         else:
             self.image = self.sprite_des.image
             self.selected = False
@@ -202,29 +202,119 @@ class AnyMenu(SubGame):
                 if (keys(event, 'up') and
                   ('U' in list[self.cursor[0]][self.cursor[1]][1])):
                     self.cursor[0] -= 1
-                    self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+                    self.group.update(list[self.cursor[0]][self.cursor[1]][0])
                 elif (keys(event, 'down') and
                   ('D' in list[self.cursor[0]][self.cursor[1]][1])):
                     self.cursor[0] += 1
-                    self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+                    self.group.update(list[self.cursor[0]][self.cursor[1]][0])
                 elif (keys(event, 'left') and
                   ('L' in list[self.cursor[0]][self.cursor[1]][1])):
                     self.cursor[1] -= 1
-                    self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+                    self.group.update(list[self.cursor[0]][self.cursor[1]][0])
                 elif (keys(event, 'right') and
                   ('R' in list[self.cursor[0]][self.cursor[1]][1])):
                     self.cursor[1] += 1
-                    self.group.update(mainmen[self.cursor[0]][self.cursor[1]][0])
+                    self.group.update(list[self.cursor[0]][self.cursor[1]][0])
                 elif event.key == K_ESCAPE:
                     self.pop_state()
-                elif event.key == K_RETURN:
+                    return
+                elif keys(event, 'next'):
+                    for sprite in self.group.sprites():
+                        trigger = sprite.trigger(list[self.cursor[0]][self.cursor[1]][0])
+                        if trigger is not None:
+                            self.newstate = trigger()
+                            self.newstate.push_state()
+                            return
+                elif keys(event, 'back'):
                     self.pop_state()
-
-                        
-
+                    return
 
 
-###Classes for Menus###
+######################################################################
+# GRADE MENU                                                         #
+######################################################################
+class GradeMenu(AnyMenu):
+    """Class for the instruction pages"""
+    def __init__(self):
+        super(GradeMenu, self).__init__(self)
+        self.initialized = False
+
+    def transition_in(self):
+        #General initialization
+        if self.initialized:
+            return
+        self.cursor = [0, 0]
+        self.initialized = True
+        self.set_layers(['main', 'allignment'])
+        self.t = 0        
+        #Background initialization
+        bg = load_image(os.path.join('data', 'spacebg.jpg'))
+        self.screen_state.set_background(bg)
+        #Font & text initialization
+        pygame.font.init()
+        self.font = pygame.font.SysFont(None,80)
+        
+
+        #Sprite initialization
+#       self.play = Button(   sel_svg = main_play_sel, SVG for selected
+#                             des_svg = main_play_des, SVG for deselected
+#                             size = ( None, 50 ),     Starting Size
+#                             callout = 'play',        Name to respond to
+#                             initsel = True,          Start selected?
+#                             trigger = None)          What to launch on click
+
+        
+        self.resume = Button( sel_svg = grades_resume_sel,
+                              des_svg = grades_resume_des,
+                              size = ( 750, None ),
+                              callout = 'resume',
+                              initsel = True,
+                              trigger = None)
+        self.first = Button(  sel_svg = grades_1_sel,
+                              des_svg = grades_1_des,
+                              size = ( 275, None ),
+                              callout = 'first',
+                              initsel = False,
+                              trigger = None)
+        self.second = Button( sel_svg = grades_2_sel,
+                              des_svg = grades_2_des,
+                              size = ( 275, None ),
+                              callout = 'second',
+                              initsel = False,
+                              trigger = None)
+        self.third = Button(  sel_svg = grades_3_sel,
+                              des_svg = grades_3_des,
+                              size = ( 275, None ),
+                              callout = 'third',
+                              initsel = False,
+                              trigger = None)
+
+        positions = []
+        sw, sh = self.screen_state.get_size()
+        
+        self.resume.rect.midtop = (sw/2, int(sh*.2))
+        self.second.rect.midtop = (sw/2, int(sh*.36))
+        self.first.rect.midright =  (int(self.second.rect.midleft[0] - (sw*.01)),
+                                     self.second.rect.midleft[1])
+        self.third.rect.midleft  =  (int(self.second.rect.midright[0] + (sw*.01)),
+                                     self.second.rect.midright[1])
+
+        
+        self.allignment = Allignment(svg=main_allign, size = (0, sh))
+        self.allignment.rect.midtop = (sw/2, 0)
+
+        #Sprite group initialization
+        self.group = Group(self.resume, self.first, self.second, self.third,
+                           self.allignment)
+
+    def main_loop(self):
+        """Run the main loop"""
+        super(GradeMenu, self).main_loop(grades)
+
+            
+######################################################################
+# Main Menu                                                          #
+######################################################################
 class MainMenu(AnyMenu):
     """Class for the instruction pages"""
     def __init__(self):
@@ -261,7 +351,7 @@ class MainMenu(AnyMenu):
                               size = ( 350, None ),
                               callout = 'play',
                               initsel = True,
-                              trigger = None)
+                              trigger = GradeMenu)
         self.howto = Button(  sel_svg = main_howto_sel,
                               des_svg = main_howto_des,
                               size = ( 562, None ),
@@ -289,11 +379,6 @@ class MainMenu(AnyMenu):
         self.credits.rect.midbottom = (sw/2, int(sh - (sh*.17)))
         self.about.rect.midbottom = (sw/2, int(sh - (sh*.05)))
 
-        print self.play.rect.midtop
-        print self.howto.rect.midtop
-        print self.about.rect.midbottom
-        print self.credits.rect.midbottom
-        
         self.allignment = Allignment(svg=main_allign, size = (0, sh))
         self.allignment.rect.midtop = (sw/2, 0)
 
@@ -304,16 +389,4 @@ class MainMenu(AnyMenu):
     def main_loop(self):
         """Run the main loop"""
         super(MainMenu, self).main_loop(mainmen)
-
-        for event in pygame.event.get():
-            if keys(event, 'next'):
-                for sprite in self.group.sprites():
-                    trigger = sprite.trigger()
-                    if trigger is not None:
-                        self.newstate = trigger()
-                        self.newstate.push_state()
-
-
-
-            
 
