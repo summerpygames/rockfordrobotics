767beeedb161c05e55219fce21ac963fbbaada2c
Mark Amber <me@markamber.co>
2011-07-29 22:53:34 -0500

    Changed the icon

--- activity.py
+++ activity.py
@@ -2,8 +2,8 @@ from olpcgames import activity
 from gettext import gettext as _
 
 class Activity(activity.PyGameActivity):
-    """Your Sugar activity"""
+    """Sugar activity launcher script"""
     
     game_name = 'launcher'
-    game_title = _('Space Math Game')
+    game_title = _('Laser Math Game')
     game_size = None
